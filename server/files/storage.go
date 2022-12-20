97af78c79f30314ba3c2919f449cd13c418de93e
takahiro.tominaga <tt.300607@gmail.com>
2022-12-21 01:43:35 +0900

    feat: アップロード対応

--- server/files/storage.go
+++ server/files/storage.go
@@ -1,6 +1,10 @@
 package files
 
+import "io"
+
 type Storage interface {
 	GetAllDirNames() ([]string, error)
 	GetAllImagePaths(userName string) ([]string, error)
+	Save(path string, file io.Reader) error
+	DeleteAll(userName string) error
 }
--- server/handlers/images.go
+++ server/handlers/images.go
@@ -1,7 +1,9 @@
 package handlers
 
 import (
+	"fmt"
 	"net/http"
+	"path/filepath"
 
 	"github.com/gin-gonic/gin"
 	"github.com/kokoichi206/action-image-manager/server/files"
@@ -15,7 +17,7 @@ type Images struct {
 
 func NewImages(s files.Storage, c util.Config) *Images {
 	return &Images{
-		store: s,
+		store:  s,
 		config: c,
 	}
 }
@@ -39,4 +41,41 @@ func (i *Images) AllImages(c *gin.Context) {
 		Images: paths,
 	}
 	c.JSON(http.StatusOK, response)
+}
+
+// https://pkg.go.dev/net/http#Request.ParseMultipartForm
+func (f *Images) UploadMultipart(c *gin.Context) {
+
+	rw := c.Writer
+	r := c.Request
+
+	err := r.ParseMultipartForm(128 * 1024)
+	if err != nil {
+		http.Error(rw, "Expected multipart form data", http.StatusBadRequest)
+		return
+	}
+
+	// from field1 to field5
+	person := r.FormValue("person")
+
+	// Delete All Files
+	// 順番ここでいいかは要検討
+	err = f.store.DeleteAll(person)
+	if err != nil {
+		fmt.Println("Error happened when deleting all files, ", err.Error())
+	}
+
+	// multipart form における value の値。
+	expectedValues := []string{"field1", "field2", "field3", "field4", "field5"}
+
+	for _, expectedValue := range expectedValues {
+		file, fh, err := r.FormFile(expectedValue)
+		if err != nil {
+			fmt.Println("Expected file ", expectedValue)
+		}
+
+		path := filepath.Join(person, fh.Filename)
+		f.store.Save(path, file)
+		file.Close()
+	}
 }
