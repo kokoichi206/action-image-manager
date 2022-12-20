ae7e93a20696e8fa464a1619dc0fc3ff61c78b15
takahiro.tominaga <tt.300607@gmail.com>
2022-12-21 01:39:27 +0900

    fix: config値が使われてなかったのを修正

--- server/util/config.go
+++ server/util/config.go
@@ -1,15 +1,15 @@
 package util
 
 type Config struct {
-	BindAddress string
+	BindAddress   string
 	ImageBasePath string
 }
 
 func NewConfig(
 	address, imageBasePath string,
 ) Config {
-	return Config {
-		BindAddress: ":9090",
-		ImageBasePath: "./imagestore",
+	return Config{
+		BindAddress:   address,
+		ImageBasePath: imageBasePath,
 	}
 }
